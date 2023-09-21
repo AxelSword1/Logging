0ab324f8600e89e55fec89ff3d3d15c6b5db05a2
AxelSword1 <127165281+AxelSword1@users.noreply.github.com>
2023-09-20 19:58:07 -0600

    Subir la clase principal

---
+++ class.py
@@ -0,0 +1,15 @@
+class Calculadora:
+    def suma(self, a, b):
+        return a + b
+
+    def resta(self, a, b):
+        return a - b
+
+    def multiplicacion(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b == 0:
+            return "¡Error! No se puede dividir entre cero."
+        else:
+            return a / b
