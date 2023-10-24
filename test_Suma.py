4bd268ba278b1630b1bcb0b3381abf2df70679c9
AxelSword1 <127165281+AxelSword1@users.noreply.github.com>
2023-10-23 19:31:49 -0600

    Update test_Suma.py

--- test_Suma.py
+++ test_Suma.py
@@ -1,4 +1,12 @@
+#libreria de logging
+import logging
 from clase import Calculadora
+
+# Configura el logging de lo que quieres mandar
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def test_suma():
     assert Calculadora.suma(3,5) == 8
+
+if __name__ == "__main__":
+    test_suma()
