7882cec3dd414edbe7e05b36e4bd31f90bc96aa0
AxelSword1 <127165281+AxelSword1@users.noreply.github.com>
2023-10-23 19:29:17 -0600

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,15 +1,28 @@
+import logging
+
 class Calculadora:
     def suma(a, b):
-        return a + b
+        result = a + b
+        #Este es un logging de nivel INFO 
+        logging.info(f'Suma: {a} + {b} = {result}')
+        return result
 
     def resta(a, b):
-        return a - b
+        result = a - b
+        logging.info(f'Resta: {a} - {b} = {result}')
+        return result
 
     def multiplicacion(a, b):
-        return a * b
+        result = a * b
+        logging.info(f'Multiplicación: {a} * {b} = {result}')
+        return result
 
     def division(a, b):
         if b == 0:
+            #Este es un logging de nivel ERROR 
+            logging.error("¡Error! No se puede dividir entre cero.")
             return "¡Error! No se puede dividir entre cero."
         else:
-            return a / b
+            result = a / b
+            logging.info(f'División: {a} / {b} = {result}')
+            return result
