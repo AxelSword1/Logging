d791b63c054568d106865c686194768396502a74
Leytor123 <127165852+Leytor123@users.noreply.github.com>
2023-10-23 20:22:17 -0600

    Update test_resta.py

--- test_resta.py
+++ test_resta.py
@@ -1,5 +1,11 @@
+import logging
 from clase import Calculadora
+
+# Configura la configuración de logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def test_resta():
     assert Calculadora.resta(10,5) == 5
 
+if __name__ == "__main__":
+    test_resta()
