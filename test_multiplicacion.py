b403e8c517322b8c24780e7d67ec2e41d752157e
Joel Valencia <joeel.valencia@gmail.com>
2023-10-23 20:00:11 -0600

    Segunda modificacion loggin

--- test_multiplicacion.py
+++ test_multiplicacion.py
@@ -1,5 +1,11 @@
+import logging
 from clase import Calculadora
 
+# Configura la configuración de logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def test_multiplicacion():
     assert Calculadora.multiplicacion(3,4) == 12
+
+if __name__ == "__main__":
+    test_multiplicacion()
